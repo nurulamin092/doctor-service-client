018ced24d53724490bbd3b16eb99479521f439a0
nurulamin092 <mnurulamin092@gmial.com>
2022-04-17 00:29:27 +0600

    update router

---
+++ src/PageNotFount/PageNotFount.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageNotFount = () => {
+    return (
+        <div className='text-warning'>
+            <h2>404</h2>
+        </div>
+    );
+};
+
+export default PageNotFount;

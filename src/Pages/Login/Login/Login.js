65fc41d04033e626dc6d4ea192f3d16c56d5a661
nurulamin092 <mnurulamin092@gmial.com>
2022-04-17 17:25:43 +0600

    reset password sent email new password

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,12 +1,16 @@
+import { async } from '@firebase/util';
 import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import auth from '../../../firebase.init';
 import CustomLink from '../../Shared/CustomLink/CustomLink';
+import Loading from '../../Shared/Loading/Loading';
 
 const Login = () => {
     let location = useLocation();
+    const navigate = useNavigate();
     let from = location.state?.from?.pathname || "/";
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -16,21 +20,33 @@ const Login = () => {
     const handlePasswordBlur = (e) => {
         setPassword(e.target.value);
     }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    }
     const [
         signInWithEmailAndPassword,
         user,
         loading,
         error,
     ] = useSignInWithEmailAndPassword(auth);
-
-    const navigate = useNavigate();
+    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
     if (user) {
         navigate(from, { replace: true });
     }
+    if (loading || sending) {
+        return <Loading></Loading>
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+    const resetPassword = async () => {
+        if (email) {
+            await sendPasswordResetEmail(email);
+            toast('send email')
+        }
+        else {
+            toast('please enter your email address');
+        }
+    }
+
     const navigateRegister = (e) => {
         navigate('/register')
     }
@@ -52,6 +68,8 @@ const Login = () => {
                     Login
                 </Button>
             </Form>
+            <p>Forget Password? <button className='btn btn-link text-primary pe-auto text-decoration-none' onClick={resetPassword}>Reset Password</button> </p>
+
             <p className='d-flex'><span className='mx-2'>Don’t have an account?</span> <CustomLink to='/register' className='text-danger pe-auto text-decoration-none' onClick={navigateRegister}>Please Register</CustomLink></p>
 
         </div>

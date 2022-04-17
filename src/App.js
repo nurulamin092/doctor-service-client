9b3e279f98dc0230185ed7663ddb61ce3eb9e9ef
nurulamin092 <mnurulamin092@gmial.com>
2022-04-17 06:15:46 +0600

    update email password firebase authentication

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from './Pages/About/About';
 import './App.css';
 import PageNotFount from './Pages/PageNotFount/PageNotFount';
 import Home from './Pages/Home/Home/Home';
+import Register from './Pages/Login/Register/Register';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/register' element={<Register></Register>}>Register</Route>
         <Route path='*' element={<PageNotFount></PageNotFount>}></Route>
 
       </Routes>
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,55 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import auth from '../../../firebase.init';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { async } from '@firebase/util';
 const Register = () => {
-    return (
-        <div>
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleNameBlur = (e) => {
+        setName(e.target.value);
+    }
+    const handleEmailBlur = (e) => {
+        setEmail(e.target.value);
+    }
+    const handlePasswordBlur = (e) => {
+        setPassword(e.target.value);
+    }
 
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        // const name = e.target.value.name;
+        /*     const email = e.target.value.email;
+            const password = e.target.value.password;
+            createUserWithEmailAndPassword(email, password); */
+    }
+    return (
+        <div className='w-50 mx-auto mt-2'>
+            <Form onSubmit={handleRegister}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control onBlur={handleNameBlur} type="text" placeholder="Enter Name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button onClick={() => createUserWithEmailAndPassword(email, password)}
+                    variant="primary" type="submit">
+                    Register
+                </Button>
+            </Form>
         </div>
     );
 };

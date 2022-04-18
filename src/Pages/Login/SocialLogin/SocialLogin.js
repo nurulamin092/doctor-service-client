02a002dbf879641afb6e49cdf4d4b11dc99ed1e3
nurulamin092 <mnurulamin092@gmial.com>
2022-04-18 06:46:33 +0600

    added social icon

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useSignInWithFacebook, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
-
+import google from '../../../images/social/google.png'
+import facebook from '../../../images/social/facebook.png'
 const SocialLogin = () => {
     const [signInWithGoogle] = useSignInWithGoogle(auth);
     const [signInWIthFacebook] = useSignInWithFacebook(auth);
@@ -10,13 +11,13 @@ const SocialLogin = () => {
             <button
                 onClick={() => signInWithGoogle()}
                 className='btn btn-info w-50 d-block mx-auto my-2'>
-                {/* <img style={{ width: '30px' }} src={google} alt="" /> */}
+                <img style={{ width: '30px' }} src={google} alt="" />
                 <span className='px-2'>Google Sign In</span>
             </button>
             <button
                 onClick={() => signInWIthFacebook()}
-                className='btn btn-info w-50 d-block mx-auto my-2'>
-                {/* <img style={{ width: '30px' }} src={google} alt="" /> */}
+                className='btn btn-primary w-50 d-block mx-auto my-2'>
+                <img style={{ width: '30px' }} src={facebook} alt="" />
                 <span className='px-2'>Facebook Sign In</span>
             </button>
         </div>
